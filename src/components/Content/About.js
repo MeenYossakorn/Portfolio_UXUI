e089264a50887847ab5b85c51abb596ade4b39c4
Yossakorn Lengrabum <meen0969987598@gmail.com>
2026-05-10 23:44:47 +0700

    Change Resume Link

--- src/components/Content/About.js
+++ src/components/Content/About.js
@@ -57,7 +57,7 @@ const About = () => {
                       </p>
                       <div className="mt-8">
                         <a
-                          href="https://drive.google.com/file/d/1G9OlUKek6LvmFUCatqMtfphgy3yVVBEG/view?usp=sharing"
+                          href="https://drive.google.com/file/d/1qKnWNlWq9XRXG0UdWH_1pbrDFivR2Zd7/view?usp=sharing"
                           target="_blank"
                           rel="noopener noreferrer"
                         >
